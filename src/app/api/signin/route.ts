7a137d004ec5b524861ea36fdbf4398fc3904df5
Jacob Sosine <jacobsosine@gmail.com>
2025-01-31 16:01:57 -0800

    added changes to sign in

--- src/app/api/signin/route.ts
+++ src/app/api/signin/route.ts
@@ -1,10 +1,29 @@
 'use server'
+import { db } from '@/server/db';
 import { NextRequest, NextResponse } from 'next/server';
+import { comparePasswords, createJWT } from '@/app/utils/auth';
 
 export async function POST(req: NextRequest) {
     const { email, password } = await req.json();
-    console.log(email, password);
-    return NextResponse.json({ message: "Sign in successful" });
-}
+    const user = await db.user.findUnique({
+        where: {
+            username: email,
+        },
+    });
+    if (!user) {
+        return NextResponse.json({ message: "User not found" });
+    }
 
+    const isValid = await comparePasswords(password, user.password);
 
+    if (!isValid) {
+        return NextResponse.json({ message: "Invalid credentials" });
+    }
+    const token = createJWT(user);
+    return NextResponse.json({
+        user: {
+            hasBackground: user.hasBackground,
+        },
+        token: token,
+    });
+};
